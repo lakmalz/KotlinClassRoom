36dfe7912e0e49ea17ef895dfa5d28444a8128e9
Lakmal Weerasekara <lakmalzweerasekara@gmail.com>
2023-10-28 17:23:31 +0800

    lifescyleScope

--- app/src/main/java/com/example/kotlinclassroom/MainActivity.kt
+++ app/src/main/java/com/example/kotlinclassroom/MainActivity.kt
@@ -1,23 +1,41 @@
 package com.example.kotlinclassroom
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private lateinit var txt: TextView
+    private lateinit var nextBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txt = findViewById(R.id.tvData)
 
+        nextBtn = this.findViewById(R.id.button)
+        nextBtn.setOnClickListener {
+            lifecycleScope.launch {
+                while (true) {
+                    delay(1000L)
+                    Log.d(TAG, "Loop")
+                }
+            }
+
+            GlobalScope.launch {
+                delay(4000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
     }
 }
