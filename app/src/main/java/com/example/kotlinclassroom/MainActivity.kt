f10e3e5d4176b1a5b9f54c4540a99f06086bb427
Lakmal Weerasekara <lakmalzweerasekara@gmail.com>
2023-10-28 18:32:06 +0800

    CoroutineExceptionHandler

--- app/src/main/java/com/example/kotlinclassroom/MainActivity.kt
+++ app/src/main/java/com/example/kotlinclassroom/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.kotlinclassroom
 
+import android.net.http.HttpException
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import java.net.HttpRetryException
+import java.net.HttpURLConnection
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -14,15 +24,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         txt = findViewById(R.id.tvData)
 
+// 1)        // This is the problem
+//        lifecycleScope.launch {
+//            try {
+//                launch {
+//                    throw Exception()
+//                }
+//            } catch (e: Exception) {
+//                println("Caught exception: ${e.message}")
+//            }
+//        }
 
-        lifecycleScope.launch {
-            try {
+        // How to handle exception
+        val handler = CoroutineExceptionHandler { ctx, throwable ->
+            println("Caught exception: $throwable")
+        }
+        CoroutineScope(Dispatchers.Main + handler).launch {
+            supervisorScope {
                 launch {
-                    throw Exception()
+                    delay(300L)
+                    throw Exception("Coroutine 1 failed")
                 }
-            } catch (e: Exception) {
-                println("Caught exception: ${e.message}")
+
+                launch {
+                    delay(400L)
+                    println("Coroutine 2 finished")
+                }
             }
+        }
+
+        // Job cancel and handle exception
+        lifecycleScope.launch {
+            val job = launch {
+                try {
+                    delay(500L)
+                }catch (e: HttpRetryException){
+                    if(e is CancellationException){
+                        throw e
+                    }
+                    e.printStackTrace()
+                }
+                println("Coroutine 1 Finished")
+            }
+
+            delay(300L)
+            job.cancel()
+        // after cancel it is going to exception
+
         }
     }
 
